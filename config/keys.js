367fb05a59927a86ecc0fcb1b932001a8d453d59
SarimDEV <m2sarim.dev@gmail.com>
2019-06-29 02:17:15 -0400

    fixing conflicts

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://web:qwertyuiop123456789@cluster0-d2ktq.mongodb.net/test?retryWrites=true&w=majority',
-    secretKey: 'octapussy'
+    mongoURI: '<mongokey>',
+    secretKey: '<some string>'
 }
